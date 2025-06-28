46e38473fab3e2fdc1be378359d316c1c7ed9d90
Thilak <murgamthilak@gmail.com>
2025-06-28 19:35:14 +0530

    third

--- index.js
+++ index.js
@@ -24,5 +24,5 @@ mongoose.connect(process.env.MONGO_URI, {
 app.use('/signups', authRoutes);
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
